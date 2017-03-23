b88bce8d932b3b56e38ad1d1cc3f960d63421335
韩英明 <hanyingming@i-etong.com>
2017-03-23 15:35:11 +0800

    add baidu map

--- WHBaiduMapAndNavigation/AppDelegate.h
+++ WHBaiduMapAndNavigation/AppDelegate.h
@@ -8,7 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>{
+    UINavigationController *navigationController;
+    BMKMapManager* _mapManager;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
--- WHBaiduMapAndNavigation/WHViewController/WHHomeViewController.m
+++ WHBaiduMapAndNavigation/WHViewController/WHHomeViewController.m
@@ -8,16 +8,53 @@
 
 #import "WHHomeViewController.h"
 
-@interface WHHomeViewController ()
+@interface WHHomeViewController ()<BMKMapViewDelegate>
+
+@property (nonatomic, assign) BMKMapView *mapView;
 
 @end
 
 @implementation WHHomeViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.title = @"HOME_Map";
+    
+    [self initAllView];
+    
+    
     // Do any additional setup after loading the view from its nib.
 }
+
+- (void)initAllView{
+//    [self.mapView setFrame:CGRectMake(0, 64, 375, 603)];
+//    self.view = _mapView;
+    
+    BMKMapView* mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 375, 603)];
+    self.view = mapView;
+    
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self.mapView viewWillAppear];
+    _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    [self.mapView viewWillDisappear];
+    _mapView.delegate = nil; // 不用时，置nil
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
